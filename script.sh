305d4e56def09eb382bb9da0a598750c3fc141a7
Thomas Nguyen <thomas1003nguyen@gmail.com>
2024-09-23 18:37:17 -0700

    script.sh updated

--- script.sh
+++ script.sh
@@ -2,20 +2,21 @@
 
 gcc fib.c -o fib
 
-echo "Nth Value,method,real_time,user_time,sys_time" > results.csv
+echo "Nth Value,method,result,real_time,user_time,sys_time" > results.csv
 
 for method in i r
 do
-    for num in {0..60} 
+    for num in {0..100} 
     do
 
-        ./fib $num $method > /dev/null
+        result=$(./fib $num $method)
+
 
         TIMEFORMAT='%R %U %S'
         exec_time=$( { time ./fib $num $method > /dev/null; } 2>&1 )
         read real_time user_time sys_time <<< $exec_time
 
         
-        echo "$num,$method,$real_time,$user_time,$sys_time" >> results.csv
+        echo "$num,$method,$result,$real_time,$user_time,$sys_time" >> results.csv
     done
 done
